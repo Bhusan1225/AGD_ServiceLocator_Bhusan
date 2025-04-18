2683e9315c2b645dd7e5449fa1619d724f3d786e
bhusanSahoo <bhusan12sahoo@gmail.com>
2025-04-18 20:10:37 +0530

    Service Locator design Pattern

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -1,5 +1,9 @@
+using ServiceLocator.Map;
 using ServiceLocator.Player;
+using ServiceLocator.Sound;
+using ServiceLocator.UI;
 using ServiceLocator.Utilities;
+using ServiceLocator.Wave;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,20 +11,48 @@ using UnityEngine;
 public class GameService : GenericMonoSingleton<GameService>
 {
 
-    //resposibilities 
-    // to get the access of services across the board.
-    // create Respected services
-    public PlayerService playerService { get; private set; }
-    [SerializeField]public PlayerScriptableObject playerScriptableObject;
 
+    public PlayerService playerService { get; private set; }
+    public SoundService soundService { get; private set; }
+    public MapService mapService { get; private set; }
+
+    public WaveService waveService { get; private set; }
+
+    
+
+    [Header("PlayerService")]
+    [SerializeField] PlayerScriptableObject playerScriptableObject;
+
+    [Header("SoundService")]
+    [SerializeField] AudioSource audioEffects;
+    [SerializeField] AudioSource backgroundMusic;
+    [SerializeField] SoundScriptableObject soundScriptableObject;
+    
+    [Header("UIService")]
+    [SerializeField] UIService uiService;
+    public UIService UIService => uiService;
+
+    [Header("MapService")]
+    [SerializeField] private MapScriptableObject mapScriptableObject;
+
+    [Header("WaveService")]
+    [SerializeField] private WaveScriptableObject waveScriptableObject;
     private void Start()
     {
         playerService = new PlayerService(playerScriptableObject);
-    }
+        soundService = new SoundService(soundScriptableObject, audioEffects, backgroundMusic);
+        mapService = new MapService(mapScriptableObject);
+        waveService =new WaveService(waveScriptableObject);
+       
 
+    }
+      
     private void Update()
     {
         playerService.Update();
+
+       
     }
+
 
 }
